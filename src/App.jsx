3044bbcebca0d7cb21f80b1cc21394b13fd18c87
PythonXHunter <aryan.computerscience@gmail.com>
2024-09-09 15:16:27 +0530

    crud using zustand

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,63 @@
+import React, { useState } from "react";
 import { usePersonStore } from "./store/person";
 
-export default function App(){
-  const {createPerson, people, addPerson, resetPerson} = usePersonStore();
-  const firstName = usePersonStore((state) => state.person.firstName);
-  const lastName = usePersonStore((state) => state.person.lastName);
-  console.log(people);
+export default function App() {
+  const { person, people, createPerson, addPerson, updatePerson, resetPerson, deletePerson } = usePersonStore();
+  const [editId, setEditId] = useState(null);
+
+  const handleSave = () => {
+    if (editId !== null) {
+      updatePerson(editId, { firstName: person.firstName, lastName: person.lastName });
+      setEditId(null); 
+    } else {
+      addPerson(person);
+    }
+    resetPerson(); 
+  };
+
+  const handleEdit = (id) => {
+    const personToEdit = people.find((p) => p.id === id);
+    if (personToEdit) {
+      createPerson(personToEdit);
+      setEditId(id); 
+    }
+  };
+
   return (
     <main>
       <label>
         First Name: {" "}
-        <input 
-          placeholder="jane"
-          value={firstName}
-          onChange={(e) => createPerson({firstName: e.target.value})}
+        <input
+          placeholder="Jane"
+          value={person.firstName}
+          onChange={(e) => createPerson({ firstName: e.target.value })}
         />
       </label>
-      <br />
-      <br />
+      <br /><br />
       <label>
         Last Name: {" "}
-        <input 
-          placeholder="doe"
-          value={lastName}
-          onChange={(e) => createPerson({lastName: e.target.value})}
+        <input
+          placeholder="Doe"
+          value={person.lastName}
+          onChange={(e) => createPerson({ lastName: e.target.value })}
         />
       </label>
-      <br />
-      <br />
-      <button onClick={() => {
-        addPerson({firstName, lastName})
-        resetPerson()
-      }}>Add</button>
-      <p>
-        Hello, <strong>{firstName} {lastName}!</strong>
-      </p>
+      <br /><br />
+      
+      <button onClick={handleSave}>
+        {editId !== null ? "Save Changes" : "Add Person"}
+      </button>
+      <button onClick={resetPerson}>Reset</button>
+
+      <h3>People List:</h3>
       <ul>
-        {people.map((person, index) => <li key={index}>
-          {person.firstName} {person.lastName}
-        </li>)}
+        {people.map((p) => (
+          <li key={p.id}>
+            {p.id}. {p.firstName} {p.lastName} {" "}
+            <button onClick={() => handleEdit(p.id)}>Edit</button>
+            <button onClick={() => deletePerson(p.id)}>Delete</button>
+          </li>
+        ))}
       </ul>
     </main>
   );
--- src/store/person.jsx
+++ src/store/person.jsx
@@ -2,17 +2,33 @@ import { create } from "zustand";
 
 export const usePersonStore = create((set) => ({
   person: {
+    id: 0,
     firstName: "",
     lastName: ""
   },
+  nextId: 1,
   people: [],
+  
   createPerson: (newPerson) => set((state) => ({
-    person: {...state.person, ...newPerson}
+    person: { ...state.person, ...newPerson }
   })),
+  
   resetPerson: () => set((state) => ({
-    person: {firstName: "", lastName: ""}
+    person: { firstName: "", lastName: "", id: state.nextId }
   })),
+
   addPerson: (person) => set((state) => ({
-    people: [...state.people, person]
+    people: [...state.people, { ...person, id: state.nextId }],
+    nextId: state.nextId + 1
+  })),
+
+  deletePerson: (id) => set((state) => ({
+    people: state.people.filter((person) => person.id !== id)
+  })),
+
+  updatePerson: (id, updatedPerson) => set((state) => ({
+    people: state.people.map((person) =>
+      person.id === id ? { ...person, ...updatedPerson } : person
+    )
   }))
-}))
+}));
