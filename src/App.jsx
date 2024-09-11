3be9c930467bef2e9be74c3335b15bd4ddb73e18
PythonXHunter <aryan.computerscience@gmail.com>
2024-09-11 22:50:39 +0530

    create people with gender field

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,168 @@
-import { useState } from "react";
-import AddPerson from "./component/AddPerson";
+import { useEffect, useState } from "react";
+import { initialPeople } from './utils/initialPeople'
 
 export default function App(){
-  const [people, setPeople] = useState([]);
-  const [currentPerson, setCurrentPerson] = useState(null);
+  const [people, setPeople] = useState(initialPeople);
+  const [currentPerson, setCurrentPerson] = useState(null)
+  
   function handleAddPerson(person){
     if(currentPerson){
-      setPeople(
-        people.map(p =>
+      setPeople(people =>
+        people.map(p => 
           p.id === currentPerson.id ? 
             {...p, ...person} : p
         )
       )
       setCurrentPerson(null);
     } else {
-      setPeople([...people, person])
+      setPeople(prevPerson => {
+        return [...prevPerson, person];
+      })
     }
   }
-  function handleDelelePerson(id){
-    setPeople(people.filter(person => person.id !== id))
+
+  function handleDeletePerson(id){
+    setPeople(prevData => 
+      prevData.filter(person => 
+        person.id !== id
+      )
+    )
   }
+
   function handleEditPerson(id){
-    const personToEdit = people.find(person => person.id === id);
-    if(personToEdit){
-      setCurrentPerson(personToEdit)
-    }
+    const personToEdit = people.find(person => person.id === id)
+    setCurrentPerson(personToEdit);
   }
-  console.log(people)
+
+  console.log(people);
+
   return (
     <div>
-      <AddPerson 
-        onAddPerson={handleAddPerson} 
+      <AddForm 
+        onAddPerson={handleAddPerson}
         updatePerson={currentPerson}
       />
-      <br />
-      <br />
-      
-      <button>Reset</button>
-      <h3>People's List:</h3>
-      <ul>
-        {people.map(person => 
-          <li key={person.id}>
-            {person.firstName} {person.lastName}
-            <button onClick={() => handleEditPerson(person.id)}>Edit</button>
-            <button onClick={() => handleDelelePerson(person.id)}>Delete</button>
-          </li>
-        )}
-      </ul>
+      <DisplayPeople 
+        people={people} 
+        onDeletePerson={handleDeletePerson}
+        onEditPerson={handleEditPerson}
+      />
     </div>
   );
+}
+
+function AddForm({onAddPerson, updatePerson}){
+  const [person, setPerson] = useState({
+    firstName: "",
+    lastName: "",
+    age: 0,
+    gender: "",
+    id: 3
+  });
+  useEffect(() => {
+    if(updatePerson){
+      setPerson({
+        ...updatePerson
+      })
+    } else {
+      setPerson({
+        ...person
+      })
+    }
+  }, [updatePerson])
+
+  function handleCreatePerson(){
+    onAddPerson(person);
+    person.id += 1;
+    setPerson({
+      ...person,
+      firstName: "",
+      lastName: "",
+      age: 0
+    })
+  }
+
+  return (
+    <>
+      <label>
+        First Name:
+        <input 
+          type="text"
+          placeholder="jane"
+          value={person.firstName}
+          onChange={(e) => setPerson({...person, firstName: e.target.value})}
+        />
+      </label>
+      <br /><br />
+      <label>
+        Last Name:
+        <input 
+          type="text"
+          placeholder="doe"
+          value={person.lastName}
+          onChange={(e) => setPerson({...person, lastName: e.target.value})}
+        />
+      </label>
+      <br /><br />
+      <label>
+        Age:
+        <input 
+          type="number"
+          placeholder="18"
+          value={person.age ? person.age : ""}
+          onChange={(e) => setPerson({...person, age: parseInt(e.target.value)})}
+        />
+      </label>
+      <br /><br />
+      <label>
+        Gender: 
+        <select 
+          value={person.gender}
+          onChange={(e) => setPerson({...person, gender: e.target.value})}
+        >
+          <option value="">Choose</option>
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+        </select>
+      </label>
+      <br /><br />
+      <button onClick={handleCreatePerson}>
+        {updatePerson ? 'Save Changes' : "Add"}
+      </button>
+    </>
+  )
+}
+
+function DisplayPeople({people, onDeletePerson, onEditPerson}){
+  const sortedPeople = [...people];
+  sortedPeople.sort((a, b) => a.age > b.age ? -1 : 1);
+  // const p = ['👦', '👨']
+  function isAdult(person){
+    switch(person.gender){
+      case 'male':
+        return '👨'
+      case 'female':
+        return '👩'
+    }
+  }
+  function isChild(person){
+    switch(person.gender){
+      case 'male':
+        return '👦'
+      case 'female':
+        return '👧'
+    }
+  }
+  return <ul>
+    {people.map(person => <li key={person.id}>
+      Gender: {person.age < 18 ? isChild(person) : isAdult(person)}
+      <br />
+      Name: {person.firstName} {person.lastName}
+      <br />
+      Age: {person.age}
+      <br/>
+      <button onClick={() => onDeletePerson(person.id)}>Delete</button>
+      <button onClick={() => onEditPerson(person.id)}>Edit</button>
+    </li>)}
+  </ul>;
 }
---
+++ src/utils/initialPeople.jsx
@@ -0,0 +1,23 @@
+export const initialPeople = [
+  {
+      "firstName": "aryan",
+      "lastName": "verma",
+      "age": 23,
+      "gender": "male",
+      "id": 1
+  },
+  {
+      "firstName": "aditya",
+      "lastName": "verma",
+      "age": 17,
+      "gender": "male",
+      "id": 2
+  },
+  {
+      "firstName": "palak",
+      "lastName": "verma",
+      "age": 20,
+      "gender": "female",
+      "id": 3
+  }
+]
